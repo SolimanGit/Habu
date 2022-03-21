7d1a8feedd9edb9ad35895d3d6ae25ea78a875cb
Soliman <Soliman.B15@gmail.com>
2022-03-21 16:15:48 +0100

    fix link

--- src/views/HomeExplorer.vue
+++ src/views/HomeExplorer.vue
@@ -59,7 +59,7 @@ export default defineComponent({
   mounted() {
     axios
       .get(
-        `https://api.mangadex.org/auth/check`
+        `https://api.mangadex.org/manga?order[followedCount]=desc&limit=30&includes[]=cover_art`
       )
       .then((res) => {
         this.items.push(...res.data.data);
